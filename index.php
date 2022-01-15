46f0aa98e54f36d4774d6afd013df7d76d3b7977
Ozh <ozh@ozh.org>
2022-01-15 18:16:41 +0100

    2022 update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang='en' xml:lang='en' xmlns="http://www.w3.org/1999/xhtml">
+<html lang='en'>
 <head>
 <meta charset="utf-8" />
     <!--
@@ -15,9 +15,9 @@
     <title>Ozh RICHARD &laquo; ozh.org</title>
     <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
-    <script src="https://code.jquery.com/ui/1.10.0/jquery-ui.js" type="text/javascript"></script>
-    <script src="js/script.js" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.0/jquery-ui.js"></script>
+    <script src="js/script.js"></script>
     <!--[if lte IE 9]>
     <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
     <style>
@@ -42,10 +42,11 @@
         'scrim' => array(
             'title'=> 'scr.im',
             'img'  => 'scrim.jpg',
-            'url'  => 'http://scr.im/',
+            'url'  => 'https://scr.im/',
             'desc' => 'A nice little site that transform your long ugly email address into a <span class="highlight">cute short URL</span> that is <span class="highlight">safe</span> and spambot-proof to share on Twitter, Facebook and other sites. Sold in 2010.',
             'type' => 'project',
         ),
+        /**
         'twtbg' => array(
             'title'=> 'Twt BG',
             'img'  => 'twtbgme.jpg',
@@ -53,13 +54,22 @@
             'desc' => 'Does your <span class="highlight">Twitter profile</span> look like poo on bigger or smaller resolutions? Cropped background, lots of white space? Check now.',
             'type' => 'project',
         ),
+        /**/
         'ozh1' => array(
             'title'=> 'Contact',
-            'img'  => 'ozh1.jpg',
+            'img'  => 'ozh6.jpg',
             'url'  => '',
             'desc' => 'If it\'s not a support question of any kind, be my guest and <span class="highlight">contact me</span>. Send an email to <span class="highlight">ozh</span> at this domain.</p><p>(for support, please refer to where you got the product)',
             'type' => 'bio',
         ),
+        'ozh7' => array(
+            'title'=> 'Talks',
+            'img'  => 'ozh7.jpg',
+            'url'  => '',
+            'desc' => 'I love to meet small or big <span class="highlight">audiences</span> and talk about subjects I deal with in life.</p><p><span class="highlight">My favorite topics</span>: team management; unit testing :)',
+            'type' => 'bio',
+        ),
+        /**
         'ozh2' => array(
             'title'=> 'Geek Skills',
             'img'  => 'ozh2.jpg',
@@ -67,14 +77,14 @@
             'desc' => 'I speak <span class="highlight">PHP</span> and <span class="highlight">jQuery</span>. I make <span class="highlight">WordPress</span> and <span class="highlight">YOURLS</span> my bitches.</p><p>If not napping in the grass I may be available for freelancing. If the project sounds fun :)',
             'type' => 'bio',
         ),
+        /**/
         'ozh3' => array(
             'title'=> 'Life, AFK',
-            'img'  => 'ozh3.jpg',
+            'img'  => 'ozh5.jpg',
             'url'  => '',
             'desc' => 'When not coding and playing <span class="highlight">Quake</span>, I try to spend time far away from computers. One of the thing I love the most is... <span class="highlight">wine</span>. Oh my god so many celestial flavors to taste and discover!',
             'type' => 'bio',
         ),
-        /**
         'ozh4' => array(
             'title'=> 'Underwater bubbles',
             'img'  => 'ozh4.jpg',
@@ -82,34 +92,32 @@
             'desc' => 'In the summer I usually say Good Bye to my computer and go for a <span class="highlight">zen dive</span>, deep down the big blue.</p><p>Best places I\'ve been to: the Red Sea and New Caledonia.',
             'type' => 'bio',
         ),
-        /**/
         'yourls' => array(
             'title'=> 'YOURLS',
             'img'  => 'yourls.jpg',
-            'url'  => 'http://yourls.org/',
-            'desc' => 'YOURLS is <span class="highlight">Your Own URL Shortener</span>, a free, Open Source self-hosted URL shrinker (like bit.ly) written in PHP. Stats, plugins, API: it has everything.',
+            'url'  => 'https://yourls.org/',
+            'desc' => 'I make YOURLS (<span class="highlight">Your Own URL Shortener</span>), the 𝑑𝑒 𝑓𝑎𝑐𝑡𝑜 standard solution in PHP to shorten links. 80+ contributors, 220K+ users, open source.',
             'type' => 'project',
         ),
         'blog' => array(
             'title'=> 'planetOzh',
             'img'  => 'blog.jpg',
-            'url'  => 'http://planetozh.com/blog/',
-            'desc' => 'My <span class="highlight">blog</span>. A bit about my life and a lot of code projects. I am somehow well known in the WordPress community and I have released a number of plugins and tutorials.',
+            'url'  => 'https://planetozh.com/blog/',
+            'desc' => 'My <span class="highlight">blog</span>. A bit about my life and a lot of code projects. I have been somehow well known in the WordPress community and have released a number of plugins and tutorials.',
             'type' => 'project',
         ),
         'fff' => array(
             'title'=> 'French Frag Factory',
             'img'  => 'fff.jpg',
-            'url'  => 'http://frenchfragfactory.net/',
-            'desc' => 'I have founded and manage the biggest French speaking community site about <span class="highlight">Quake</span>.</p><p><em>Est. 1999</em>. Not for the noobz.',
+            'url'  => 'https://frenchfragfactory.net/',
+            'desc' => 'I have founded and managed the biggest French speaking community site about <span class="highlight">Quake</span>.</p><p><em>Est. 1999</em>. Not for the noobz.',
             'type' => 'project',
         ),
-        /**
         'lastfm' => array(
             'title'=> 'last.fm',
             'img'  => 'lastfm.jpg',
-            'url'  => 'http://last.fm/user/-ozh-',
-            'desc' => 'My profile on <span class="highlight">Last.fm</span>, the best music site around, definitely.</p><p>Favorite band of all time: <span class="highlight">Suicidal Tendencies</span>.',
+            'url'  => 'https://last.fm/user/-ozh-',
+            'desc' => 'My profile on <span class="highlight">Last.fm</span>, for those who dig music and stats.</p><p>Favorite bands of all time: <span class="highlight">Suicidal Tendencies</span>, <span class="highlight">Be\'lakor</span>.',
             'type' => 'presence',
         ),
         /**/
@@ -121,19 +129,20 @@
             'type' => 'presence',
         ),
         'plugindevbook' => array(
-            'title'=> '<a href="http://amzn.to/plugindevbook"><img id="plugindevbook-cover" src="img/plugindevbook-cover.jpg" alt="WordPress Plugin Development"></a>',
+            'title'=> '<a href="https://amzn.to/plugindevbook"><img id="plugindevbook-cover" src="img/plugindevbook-cover.jpg" alt="WordPress Plugin Development"></a>',
             'img'  => 'plugindevbook-logo.jpg',
             'url'  => '',
-            'desc' => 'I wrote a book! Hobbyist, amateurs, web agencies, freelancers: all about <strong><a href="http://amzn.to/plugindevbook">WordPress Plugin Development</a></strong>',
+            'desc' => 'I wrote a book! Hobbyist, amateurs, web agencies, freelancers: all about <strong><a href="https://amzn.to/plugindevbook">WordPress Plugin Development</a></strong>',
             'type' => 'project',
         ),
         'twitter' => array(
             'title'=> '@ozh',
-            'img'  => 'twitter.jpg',
-            'url'  => 'http://twitter.com/ozh',
+            'img'  => 't.jpg',
+            'url'  => 'https://twitter.com/ozh',
             'desc' => 'My <span class="highlight">Twitter account</span>. Mostly about WordPress and YOURLS, and the everyday geekitude.',
             'type' => 'presence',
         ),
+        /**
         'facebook' => array(
             'title'=> 'Facebook',
             'img'  => 'facebook.png',
@@ -141,18 +150,19 @@
             'desc' => 'Yeah, of course, I\'m on Facebook. I\'m not very active there and that is mostly for people que je connais pour de vrai but feel free to say "hi!"',
             'type' => 'presence',
         ),
+        /**/
         'plugins' => array(
             'title'=> 'WordPress Plugins',
             'img'  => 'plugins.png',
-            'url'  => 'http://profiles.wordpress.org/ozh',
+            'url'  => 'https://profiles.wordpress.org/ozh',
             'desc' => 'I released a lot of <span class="highlight">free plugins</span> to make your everyday life with WordPress easier. More than <span class="highlight">one million downloads</span>!',
             'type' => 'project',
         ),
         'tshirt' => array(
             'title'=> 'T-Shirts',
             'img'  => 'tshirts.png',
-            'url'  => 'http://www.redbubble.com/people/ozhy',
-            'desc' => 'For fun I create <span class="highlight">t-shirts</span>. It started as a few geek stuff made for myself, ended up as several collections. Quality products from RedBubble.',
+            'url'  => 'https://www.redbubble.com/people/ozhy',
+            'desc' => 'For fun I create <span class="highlight">t-shirts</span>. It started as a few geek stuff made for myself and the love of Git. Quality products from RedBubble.',
             'type' => 'project',
         ),
         
@@ -184,10 +194,6 @@
 
 <div id="footer">
 
-    <div id="likebutton" class="more">
-    <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fozh.org%2F&amp;layout=standard&amp;show_faces=false&amp;width=400&amp;action=like&amp;font=verdana&amp;colorscheme=light&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:400px; height:35px; color:white;" allowTransparency="true"></iframe>
-    </div>
-
     <div id="display_controls" class="more">
         <p>Too much stuff? Display type: 
         <span class="shown">project</span>
@@ -199,11 +205,11 @@
 </div>
 
 
-<script type="text/javascript">
+<script>
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
-<script type="text/javascript">
+<script>
 try {
 var pageTracker = _gat._getTracker("UA-55088-10");
 pageTracker._trackPageview();
